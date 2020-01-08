bb1659ed2a24383c2ce4a4fa571df746e2f0d393
Thandar Htun <tandarkale3333@gmail.com>
2020-01-08 04:37:06 -0500

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+use App\Http\Resources\Post as PostResource;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+
+     */
+     public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    public function index()
+    {
+       $posts=Post::all();
+       return $posts;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   
+        $request->validate([
+            'title'=>'required|min:5',
+            'body'=>'required|min:10'
+        ]);
+        $post=new Post;
+        $post->title=request('title');
+        $post->body=request('body');
+        $post->save();
+        return $post;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         $request->validate([
+            'title'=>'required|min:5',
+            'body'=>'required|min:10'
+        ]);
+        $post=Post::find($id);
+        $post->title=request('title');
+        $post->body=request('body');
+        $post->save();
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post=Post::find($id);
+        
+        $post->delete();
+        return $post;
+    }
+}
